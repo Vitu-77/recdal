8ed5d20d22342d53b62ba5c205e7766c0dadedf9
vitu-77 <marcelo.victor05@gmail.com>
2020-06-23 15:45:49 -0300

    add: modal styles, including animations based on the transition prop

--- src/lib/styles.ts
+++ src/lib/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { OverlayProps, ModalProps } from './types';
 
@@ -23,4 +23,51 @@ export const Overlay = styled.div<OverlayProps>`
 		`};
 `;
 
-export const Modal = styled.div<ModalProps>``;
+export const Modal = styled.div<ModalProps>`
+	transition-timing-function: ${({ transition }) => transition?.timingFunction || 'linear'};
+	margin: 0;
+	padding: 0;
+	width: max-content;
+	height: max-content;
+	z-index: 3;
+	transition-duration: ${({ transition }) => transition?.duration || 300}ms;
+	transition-delay: ${({ transition }) => (transition?.duration || 300) - 100}ms;
+	background: transparent;
+
+	${({ show, transition }) => {
+		if (show) {
+			return css`
+				transform: scale(1) translate(0, 0);
+			`;
+		}
+		switch (transition?.type) {
+			case 'fade':
+				return css`
+					transform: scale(0);
+				`;
+			case 'slide':
+				switch (transition?.direction) {
+					case 'bottom-top':
+						return css`
+							transform: translate(0, calc(100vh + 200%));
+						`;
+					case 'top-bottom':
+						return css`
+							transform: translate(0, calc(-100vh - 200%));
+						`;
+					case 'right-left':
+						return css`
+							transform: translate(calc(100vw + 200%), 0);
+						`;
+					case 'left-right':
+						return css`
+							transform: translate(calc(-100vw - 200%), 0);
+						`;
+					default:
+						return null;
+				}
+			default:
+				return null;
+		}
+	}};
+`;
