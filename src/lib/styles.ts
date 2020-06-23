2a21c5ab28be1c14c881806d77512e513aa8d010
vitu-77 <marcelo.victor05@gmail.com>
2020-06-23 15:44:38 -0300

    add: overlay styles

--- src/lib/styles.ts
+++ src/lib/styles.ts
@@ -1,24 +1,26 @@
 import styled from 'styled-components';
 
-interface OverlayProps {
-	top: number;
-	show: boolean;
-	background?: string;
-	transitionDuration: number;
-}
-
-interface ModalProps {
-	show: boolean;
-	styles?: {
-		boxShadow?: string;
-		background?: string;
-	};
-}
+import { OverlayProps, ModalProps } from './types';
 
 export const Overlay = styled.div<OverlayProps>`
+	position: absolute;
+	left: 0vw;
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	z-index: 2;
+	opacity: 0;
+	transition: ${({ transitionDuration }) => transitionDuration}ms;
+	top: ${({ top }) => top}px;
+	background: ${({ background }) => background || 'rgba(50, 50, 50, 0.3)'};
 
+	${({ show }) =>
+		show &&
+		css`
+			opacity: 1;
+		`};
 `;
 
-export const Modal = styled.div<ModalProps>`
-
-`;
+export const Modal = styled.div<ModalProps>``;
