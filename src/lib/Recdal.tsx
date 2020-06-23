22e24c59ffefe24990ba68d0feba01fa6480ef8b
vitu-77 <marcelo.victor05@gmail.com>
2020-06-23 11:47:23 -0300

    add: Recdal props and handlers interfaces

--- src/lib/Recdal.tsx
+++ src/lib/Recdal.tsx
@@ -1,13 +1,14 @@
-import * as React from 'react';
 import OutsideClickHandler from 'react-outside-click-handler';
+import React from 'react';
+
+import { Props, ModalHandlers } from './types';
 
 import { Overlay, Modal } from './styles';
 
-interface Props {
-	children: any;
-}
-
-const Recdal: React.FC<Props> = ({ children }) => {
+const Recdal: React.RefForwardingComponent<ModalHandlers, Props> = (
+	{ initialData = {}, defaultVisible = false, closeOnOverlayClick = true, ...rest },
+	ref
+) => {
 	return (
 		<Overlay>
 			<Modal>{children}</Modal>
